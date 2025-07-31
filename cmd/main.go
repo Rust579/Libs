eb6acb2d817eaa0cff560d32c459bfdca341829e
Rust579 <Rustem3494@gmail.com>
2025-07-31 14:29:32 +0500

    aaa

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"Libs/configs"
-	"Libs/grpc/uniapi"
-	"context"
-	"github.com/sirupsen/logrus"
-	"os/signal"
-	"syscall"
-)
-
-var gfsd = make(chan bool)
+import "fmt"
 
 func main() {
-
-	err := configs.Init()
-	if err != nil {
-		logrus.Fatalf("error open config file: %s", err.Error())
-	}
-
-	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-
-	//uniapi.GoRunTest()
-	//uniapi.DockerRunTest()
-	//uniapi.JSRunTest()
-	uniapi.PyRunTest()
-
-	<-ctx.Done()
+	fmt.Println("fff")
 }
